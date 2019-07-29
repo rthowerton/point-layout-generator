8f7e80eaaecba76eacb9ee5ee0c58080f2f30579
rthowerton <howertor@oregonstate.edu>
2019-07-29 14:32:15 -0700

    Translated circle map logic to C#

---
+++ testing_code/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace AlgorithmTest
+{
+    class Program
+    {
+        private const int radius = 150;
+        static void Main(string[] args)
+        {
+            Square[,] squares = new Square[radius*2, radius*2];
+            int x = 0, y = 0;
+            for(int i = -150; i < 150; i++)
+            {
+                y = 0;
+                for(int j = -150; j < 150; j++)
+                {
+                    Point bl = new Point(i, j);
+                    Point br = new Point(i + 2, j);
+                    Point tl = new Point(i, j + 2);
+                    Point tr = new Point(i + 2, j + 2);
+                    Point center = new Point(i + 1, j + 1);
+                    Square s = new Square(tl, tr, bl, br, center);
+                    s.isValid();
+                    squares[x, y] = s;
+                    y++;
+                }
+                x++;
+            }
+
+            Console.Write("[{0},{1}]\n", squares[x / 2, y / 2].getCenter().x, squares[x/2,y/2].getCenter().y);
+            Console.Write("{0}\n", squares[x / 2, y / 2].getValid());
+            Console.Write("[{0},{1}]\n", squares[0, 0].getCenter().x, squares[0, 0].getCenter().y);
+            Console.Write("{0}\n", squares[0, 0].getValid());
+        }
+    }
+}
---
+++ testing_code/Square.cs
@@ -0,0 +1,114 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace AlgorithmTest
+{
+    public struct Point
+    {
+        public int x, y;
+
+        public Point(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+    }
+    class Square
+    {
+        private Point tl, tr, bl, br, center;
+        private bool valid;
+
+        public Square() { }
+
+        public Square(Point a, Point b, Point c, Point d, Point e)
+        {
+            tl = a;
+            tr = b;
+            bl = c;
+            br = d;
+            center = e;
+        }
+
+        public void setCenter(Point c)
+        {
+            center = c;
+        }
+
+        public void setTopLeft(Point c)
+        {
+            tl = c;
+        }
+
+        public void setTopRight(Point c)
+        {
+            tr = c;
+        }
+
+        public void setBotLeft(Point c)
+        {
+            bl = c;
+        }
+
+        public void setBotRight(Point c)
+        {
+            br = c;
+        }
+
+        public Point getCenter()
+        {
+            return center;
+        }
+
+        public Point[] getCorners()
+        {
+            Point[] corners = new Point[4];
+            corners[0] = tl;
+            corners[1] = tr;
+            corners[2] = bl;
+            corners[3] = br;
+            return corners;
+        }
+
+        // Check to see if a square is wholly within the 150mm radius
+        public void isValid()
+        {
+            // Sides of the triangle whose hypoteneuse is 150mm from [0,0]
+            int a = 0, b = 0;
+            // Determine which quadrant the square is in
+            // and account for negative values
+            if (center.x > 0 && center.y > 0)
+            {
+                b = tr.x;
+            }
+            else if (center.x < 0 && center.y > 0)
+            {
+                a = tl.y;
+                b = tr.x * -1;
+            }
+            else if (center.x < 0 && center.y < 0)
+            {
+                a = bl.y * -1;
+                b = bl.x * -1;
+            }
+            else if (center.x > 0 && center.y < 0)
+            {
+                a = br.y * -1;
+                b = br.x;
+            }
+
+            // Perform the check
+            if ((a * a) + (b * b) > (150 * 150))
+                valid = false;
+            else
+                valid = true;
+        }
+
+        public bool getValid()
+        {
+            return valid;
+        }
+    }
+}
