c66baf99bb796deb4aeefd9e206f44dc2092d96f
rthowerton <howertor@oregonstate.edu>
2019-07-30 21:27:07 -0700

    Now with comments for clarity.

--- PointLayoutGenerator/Die.cs
+++ PointLayoutGenerator/Die.cs
@@ -2,6 +2,7 @@
 
 namespace PointLayoutGenerator
 {
+    /* Struct to represent a single [x,y] coordinate */
     public struct Point
     {
         public float x, y;
@@ -15,11 +16,18 @@ namespace PointLayoutGenerator
 
     class Die
     {
+        /* A die is composed of 5 points, to symbolize its 
+         * corners and the center. */
         private Point tl, tr, bl, br, center;
+
+        /* And also a valid flag to check whether the die
+         * is within the 150mm radius. */
         private bool valid;
 
+        /* Generic constructor */
         public Die() { }
 
+        /* Constructor */
         public Die(Point a, Point b, Point c, Point d, Point e)
         {
             tl = a;
@@ -29,36 +37,42 @@ namespace PointLayoutGenerator
             center = e;
         }
 
+        /* Set the center point value */
         public void setCenter(Point c)
         {
             center = c;
         }
-
+        /* Set the top left point value */
         public void setTopLeft(Point c)
         {
             tl = c;
         }
 
+        /* Set the top right point value */
         public void setTopRight(Point c)
         {
             tr = c;
         }
 
+        /* Set the bottom left point value */
         public void setBotLeft(Point c)
         {
             bl = c;
         }
 
+        /* Set the bottom right point value */
         public void setBotRight(Point c)
         {
             br = c;
         }
 
+        /* Get just the center point */
         public Point getCenter()
         {
             return center;
         }
 
+        /* Get all of the corner values */
         public Point[] getCorners()
         {
             Point[] corners = new Point[4];
@@ -69,13 +83,13 @@ namespace PointLayoutGenerator
             return corners;
         }
 
-        // Check to see if a square is wholly within the 150mm radius
+        /* Check to see if a square is wholly within the 150mm radius */
         public void isValid()
         {
             Point[] corners = this.getCorners();
             foreach (Point corner in corners)
             {
-                // Perform the check
+                // Pythagorean theorem
                 if ((corner.x * corner.x) + (corner.y * corner.y) > (Generator.radius * Generator.radius))
                 {
                     valid = false;
@@ -86,6 +100,7 @@ namespace PointLayoutGenerator
             }
         }
 
+        /* Return the valid flag */
         public bool getValid()
         {
             return valid;
--- PointLayoutGenerator/Generator.cs
+++ PointLayoutGenerator/Generator.cs
@@ -12,20 +12,20 @@ namespace PointLayoutGenerator
 {
     public partial class Generator : Form
     {
-        // Radius of wafer
+        /* Radius of wafer */
         public const int radius = 150;
 
-        // Array to hold a square grid of dice
+        /* Array to hold a square grid of dice */
         private Die[,] dice;
 
-        // Variables to track dimensions of the grid
+        /* Variables to track dimensions of the grid */
         private int dieWidth, dieHeight, gridDimensionX, gridDimensionY;
         private float[] xLowHigh, yLowHigh;
         
-        // Variables to track location in grid
+        /* Variables to track location within grid */
         private int xCoord, yCoord;
 
-        // Layout Strategy Tracker
+        /* Layout Strategy Trackers */
         private string layoutStrategy;
         private int numPoints;
 
@@ -34,6 +34,12 @@ namespace PointLayoutGenerator
             InitializeComponent();
         }
 
+        /* When loading the form, set the possible layouts
+         * (in the dropdown) to the two strategies. Then,
+         * set the current layout to the first strategy.
+         * Following, set both the width and height values
+         * of the layout to their minimum value of 2
+         */
         private void Generator_Load(object sender, EventArgs e)
         {
             string[] layouts = { "Rectangular", "Circular" };
@@ -43,6 +49,11 @@ namespace PointLayoutGenerator
             dieHeightInput.Value = dieHeightInput.Minimum;
         }
 
+        /* When the layout dropdown selection is changed,
+         * depending on the index, hide the other option's
+         * corresponding numeric input, show the current
+         * numeric input, and take note of the inputs.
+         */
         private void LayoutOptionsBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             if(layoutOptionsBox.SelectedIndex == 0)
@@ -62,6 +73,12 @@ namespace PointLayoutGenerator
             layoutStrategy = layoutOptionsBox.SelectedItem.ToString();
         }
 
+        /* When the die width input is changed, perform
+         * calculations on the new value (specifically,
+         * calculate the number of die in the X direction,
+         * and calculate the centerpoint of the left-most
+         * and right-most complete dice.
+         */
         private void DieWidthInput_ValueChanged(object sender, EventArgs e)
         {
             dieWidth = (int)dieWidthInput.Value;
@@ -69,6 +86,12 @@ namespace PointLayoutGenerator
             xLowHigh = getLowHigh(dieWidth);
         }
 
+        /* When the die height input is changed, perform
+         * calculations on the new value (specifically,
+         * calculate the number of die in the Y direction,
+         * and calculate the centerpoint of the top-most
+         * and bottom-most complete dice.
+         */
         private void DieHeightInput_ValueChanged(object sender, EventArgs e)
         {
             dieHeight = (int)dieHeightInput.Value;
@@ -76,16 +99,26 @@ namespace PointLayoutGenerator
             yLowHigh = getLowHigh(dieHeight);
         }
 
+        /* When the number of points the user wants is
+         * changed, immediately take note of that value.
+         * Just in case.
+         */
         private void NumPointsRectangle_ValueChanged(object sender, EventArgs e)
         {
             numPoints = (int)numPointsRectangle.Value;
         }
 
+        /* See above */
         private void NumPointsCircle_ValueChanged(object sender, EventArgs e)
         {
             numPoints = (int)numPointsCircle.Value;
         }
 
+        /* Get the total number of dice along the
+         * X-axis, ensuring that the value is always
+         * odd, so that the center die is always at
+         * [0,0].
+         */
         private int getXDimension(int dieWidth)
         {
             if (radius % dieWidth == 0)
@@ -98,6 +131,11 @@ namespace PointLayoutGenerator
             }
         }
 
+        /* Get the total number of dice along the
+         * Y-axis, ensuring that the value is always
+         * odd, so that the center die is always at
+         * [0,0].
+         */
         private int getYDimension(int dieHeight)
         {
             if (radius % dieHeight == 0)
@@ -110,6 +148,15 @@ namespace PointLayoutGenerator
             }
         }
 
+        /* Based on the axis supplied to the function,
+         * returns the minimum and maximum values for
+         * the inevitable for-loop. If the width or
+         * height supplied cleanly divides into the 150mm
+         * radius, then the min and max are the radius.
+         * Otherwise, find the next largest number that 
+         * the width or height cleanly divides into, and
+         * return that.
+         */
         private float[] getLowHigh(int dieDimension)
         {
             float[] toReturn = new float[2];
@@ -131,6 +178,10 @@ namespace PointLayoutGenerator
             }
         }
 
+        /* Based on the valid flag, prints a point
+         * on the chart dieVisualizer corresponding
+         * to the current die's center point.
+         */
         private void showValidDie(Die d)
         {
             if (d.getValid())
@@ -139,6 +190,11 @@ namespace PointLayoutGenerator
             }
         }
 
+        /* Based on the valid flag, highlight the
+         * point passed in to show that that point
+         * is going to be inspected, and add the
+         * coordinates of the point to the text box.
+         */
         private void showInspectedDie(Die d)
         {
             if (d.getValid())
@@ -150,36 +206,50 @@ namespace PointLayoutGenerator
             }
         }
 
+        /* When the plotWafer button is clicked,
+         * this function takes in the user input
+         * and prints the center points of all
+         * valid dice to the dieVisualizer chart.
+         */
         private void PlotButton_Click(object sender, EventArgs e)
         {
+            // Clear all points on the old wafer
             dieVisualizer.Series[0].Points.Clear();
             dieVisualizer.Series[1].Points.Clear();
 
             dice = new Die[gridDimensionX, gridDimensionY];
 
+            // Set the gridlines to show the edges of the dice.
+            // Has the added effect of further cluttering the chart
+            // for purposes of obfuscation
             dieVisualizer.ChartAreas[0].AxisX.Interval = dieWidth;
             dieVisualizer.ChartAreas[0].AxisX.IntervalOffset = dieWidth / 2;
             dieVisualizer.ChartAreas[0].AxisY.Interval = dieHeight;
             dieVisualizer.ChartAreas[0].AxisY.IntervalOffset = dieHeight / 2;
 
-            // Fill in array
+            // Fill in array, using xCoord and yCoord to reference the
+            // array later.
             xCoord = 0;
             for (float i = xLowHigh[0]; i <= xLowHigh[1]; i += dieWidth)
             {
                 yCoord = 0;
                 for (float j = yLowHigh[0]; j <= yLowHigh[1]; j += dieHeight)
                 {
+                    // Create 5 new points that define the current die
                     Point bl = new Point(i - (float)(dieWidth / 2), j - (float)(dieHeight / 2));
                     Point br = new Point(i + (float)(dieWidth / 2), j - (float)(dieHeight / 2));
                     Point tl = new Point(i - (float)(dieWidth / 2), j + (float)(dieHeight / 2));
                     Point tr = new Point(i + (float)(dieWidth / 2), j + (float)(dieHeight / 2));
                     Point center = new Point(i, j);
 
+                    // Create the new die and validate it
                     Die d = new Die(tl, tr, bl, br, center);
                     d.isValid();
 
+                    // If the die is valid, it will be printed
                     showValidDie(d);
 
+                    // Actually add the die to the array
                     dice[xCoord, yCoord] = d;
                     yCoord++;
                 }
@@ -189,14 +259,24 @@ namespace PointLayoutGenerator
             //testCoords();
         }
 
+        /* If this is the first button clicked, it will
+         * do the same as the plotWafer button click,
+         * generating an initial array. It will then highlight
+         * inspection points on the wafer. If it is not
+         * the first button clicked, it will just take
+         * the current array and highlight the inspection
+         * points based on user input.
+         */
         private void GenerateButton_Click(object sender, EventArgs e)
         {
+            // Clear just the current inspection points
             dieVisualizer.Series[1].Points.Clear();
 
             if (dice == null)
             {
                 dice = new Die[gridDimensionX, gridDimensionY];
 
+                // Set the gridlines to show the edges of the dice.
                 dieVisualizer.ChartAreas[0].AxisX.Interval = dieWidth;
                 dieVisualizer.ChartAreas[0].AxisX.IntervalOffset = dieWidth / 2;
                 dieVisualizer.ChartAreas[0].AxisY.Interval = dieHeight;
@@ -204,25 +284,33 @@ namespace PointLayoutGenerator
 
                 // Fill in array
                 xCoord = 0;
+                // For the current release of the app,
+                // ic and jc exist to evenly space inspection points.
                 int ic = 0, jc = 0;
                 for (float i = xLowHigh[0]; i <= xLowHigh[1]; i += dieWidth)
                 {
                     yCoord = 0;
                     for (float j = yLowHigh[0]; j <= yLowHigh[1]; j += dieHeight)
                     {
+                        // Create 5 new points that define the current die
                         Point bl = new Point(i - (float)(dieWidth / 2), j - (float)(dieHeight / 2));
                         Point br = new Point(i + (float)(dieWidth / 2), j - (float)(dieHeight / 2));
                         Point tl = new Point(i - (float)(dieWidth / 2), j + (float)(dieHeight / 2));
                         Point tr = new Point(i + (float)(dieWidth / 2), j + (float)(dieHeight / 2));
                         Point center = new Point(i, j);
 
+                        // Create the new die and validate it
                         Die d = new Die(tl, tr, bl, br, center);
                         d.isValid();
 
+                        // If the die is valid, it will be printed
                         showValidDie(d);
+                        // Check to make sure that the point is evenly spaced
+                        // then highlight it if it is valid
                         if (ic % numPoints == 0 && jc % numPoints == 0)
                             showInspectedDie(d);
 
+                        // Actually add the die to the array
                         dice[xCoord, yCoord] = d;
                         yCoord++;
                         jc++;
@@ -235,6 +323,8 @@ namespace PointLayoutGenerator
             }
             else
             {
+                // Simply run through the current array
+                // and highlight dice at the correct interval
                 int ic = 0, jc = 0;
                 for (int i = 0; i < xCoord; i++)
                 {
@@ -253,6 +343,10 @@ namespace PointLayoutGenerator
             }
         }
 
+        /* This function just builds some strings of
+         * coordinates and valid checks and prints
+         * them to the text box, like a unit test.
+         */
         private void testCoords()
         {
             string test1 = "[" + dice[xCoord / 2, yCoord / 2].getCenter().x.ToString() + "," +
